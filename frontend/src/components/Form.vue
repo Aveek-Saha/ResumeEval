9f8304d600aa5187e2b3bdc25aa2598eeda0ef7c
Aveek Saha <aveek.s98@gmail.com>
2019-10-08 16:32:41 +0530

    Enable adding multiple skills

--- frontend/src/components/Form.vue
+++ frontend/src/components/Form.vue
@@ -4,24 +4,24 @@
     <h4 class="card-title">Personal Info</h4>
       <div class="form-group">
         <label >Full Name</label>
-        <input type="text" class="form-control"   placeholder="Enter your name">
+        <input type="text" class="form-control" v-model="name"  placeholder="Enter your name">
       </div>
       <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        <label>Email address</label>
+        <input type="email" class="form-control" v-model="email" placeholder="Enter email">
         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
       </div>
       <div class="form-group">
         <label >Phone number</label>
-        <input type="tel" class="form-control"   placeholder="Enter Number">
+        <input type="tel" class="form-control" v-model="number" placeholder="Enter Number">
       </div>
       <div class="form-group">
         <label >Location</label>
-        <input type="text" class="form-control"   placeholder="Location">
+        <input type="text" class="form-control" v-model="location" placeholder="Location">
       </div>
       <div class="form-group">
         <label >Link</label>
-        <input type="text" class="form-control"   placeholder="Enter a URL">
+        <input type="text" class="form-control" v-model="link" placeholder="Website link">
       </div>
       <button class="btn btn-primary" @click.prevent="next()">Next</button>
 
@@ -38,31 +38,31 @@
         <div v-for="(school, index) in education" v-bind:key="index">
           <div class="form-group">
             <label >School Name</label>
-            <input type="text" class="form-control" v-model="school.name" placeholder="Enter your name">
+            <input type="text" class="form-control" v-model="school.name" placeholder="School/University name">
           </div>
           <div class="form-group">
-            <label >School Location</label>
-            <input type="email" class="form-control" aria-describedby="emailHelp" v-model="school.location" placeholder="Enter email">
+            <label >Location</label>
+            <input type="text" class="form-control" v-model="school.location" placeholder="School location">
           </div>
           <div class="form-group">
             <label>Degree</label>
-            <input type="tel" class="form-control" v-model="school.degree" placeholder="Enter Number">
+            <input type="tel" class="form-control" v-model="school.degree" placeholder="Degree awarded">
           </div>
           <div class="form-group">
             <label >Major</label>
-            <input type="text" class="form-control" v-model="school.major" placeholder="Location">
+            <input type="text" class="form-control" v-model="school.major" placeholder="Major">
           </div>
           <div class="form-group">
             <label >GPA</label>
-            <input type="number" class="form-control" v-model="school.gpa" placeholder="Enter a URL">
+            <input type="number" class="form-control" v-model="school.gpa" placeholder="Grade point average">
           </div>
           <div class="form-group">
             <label >Start date</label>
-            <input type="text" class="form-control" v-model="school.start" placeholder="Location">
+            <input type="text" class="form-control" v-model="school.start" placeholder="Date started">
           </div>
           <div class="form-group">
             <label >End date</label>
-            <input type="text" class="form-control" v-model="school.end" placeholder="Location">
+            <input type="text" class="form-control" v-model="school.end" placeholder="Date ended">
           </div>
           <div class="btn-group" role="group" style="text-align: right">
             <button class="btn btn-outline-success" @click.prevent="addEducation()">Add</button>
@@ -119,13 +119,20 @@
 
   <div v-show="step === 4">
     <h4 class="card-title">Skills</h4>
-      <div class="form-group">
-        <label >Skill Name</label>
-        <input type="text" class="form-control"   placeholder="Enter your name">
-      </div>
-      <div class="form-group">
-        <label for="exampleInputEmail1">Details</label>
-        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+      <div v-for="(skill, index) in skills" v-bind:key="index">
+        <div class="form-group">
+          <label >Name</label>
+          <input type="text" class="form-control" v-model="skill.name" placeholder="Skill name">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Description</label>
+          <textarea class="form-control" rows="3" v-model="skill.description" placeholder="Enter each point on a new line"></textarea>
+        </div>
+        <div class="btn-group" role="group" style="text-align: right">
+            <button class="btn btn-outline-success" @click.prevent="addSkill()">Add</button>
+            <button class="btn btn-outline-danger" @click.prevent="delSkill(index)">Del</button>
+          </div>
+        <hr>
       </div>
       
       <div class="btn-group" role="group">
@@ -198,6 +205,7 @@ export default {
       step:1,
       name:null,
       email:null,
+      number: null,
       location: null,
       link: null,
       education: [{
@@ -215,14 +223,17 @@ export default {
           location: null,
           start: null,
           end: null,
-          responsibilities: []
+          responsibilities: null
       }],
-      skills: [],
+      skills: [{
+        name: null,
+        description: null
+      }],
       projects: [{
           name: null,
           description: null,
           link: null,
-          tools: []
+          tools: null
       }],
       awards: [{
           name: null,
@@ -262,13 +273,25 @@ export default {
         location: null,
         start: null,
         end: null,
-        responsibilities: []
+        responsibilities: null
       })
     },
     delWork(index){
       if(this.work.length == 1)
         return;
       this.work.splice(index, 1)
+      
+    },
+    addSkill(){
+      this.skills.push({
+        name: null,
+        description: null
+      })
+    },
+    delSkill(index){
+      if(this.skills.length == 1)
+        return;
+      this.skills.splice(index, 1)
       
     },
   }
